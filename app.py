e28573dda204d8fe2ab00341b56f46c1cdd99564
Mayara Sebastiao <mayara.psebastiao@gmail.com>
2023-07-20 16:40:16 -0300

    aula03

--- app.py
+++ app.py
@@ -16,3 +16,19 @@ print(10 / 3)
 print(10 // 3)
 print(10 % 3)
 print(2 ** 3)
+
+print((3+5)*(7+3))
+
+print(abs(-15))
+print(pow(3,3))
+print(max(1,85,17,-55,5))
+print(min(1,85,17,-55,5))
+print(round(8.3))
+print(round(8.8))
+
+import math
+
+print(math.floor(8.8))
+print(math.ceil(8.8))
+print(math.sqrt(9))
+
